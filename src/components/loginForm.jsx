9eaf0815be516e995c5315502e1dac8e656a4c12
Jason Lord <jasonjohnlord@gmail.com>
2020-02-25 17:08:17 -0500

    added onChange validation to the input fields

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -16,10 +16,23 @@ class LoginForm extends Component {
     console.log("no errors so I can submit to the server or whatever");
   };
 
-  handleChange = e => {
+  handleChange = ({ currentTarget: input }) => {
+    const errors = { ...this.state.errors };
+    const errorMessage = this.validateProperty(input);
+    if (errorMessage) errors[input.name] = errorMessage;
+    else delete errors[input.name];
+
     const account = { ...this.state.account };
-    account[e.currentTarget.name] = e.currentTarget.value;
-    this.setState({ account });
+    account[input.name] = input.value;
+    this.setState({ account, errors });
+  };
+  validateProperty = ({ name, value }) => {
+    if (name === "username") {
+      if (value.trim() === "") return "Username is Required.";
+    }
+    if (name === "password") {
+      if (value.trim() === "") return "Password is Required.";
+    }
   };
   validate = () => {
     const errors = {};

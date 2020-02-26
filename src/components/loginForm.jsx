908aad265b20a24ac4c688af03ddb7a8a12f0d2d
Jason Lord <jasonjohnlord@gmail.com>
2020-02-26 12:11:11 -0500

    added the joi validation

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Input from "./input";
+import Joi from "joi-browser";
 
 class LoginForm extends Component {
   state = {
@@ -7,13 +8,21 @@ class LoginForm extends Component {
     errors: {}
   };
 
+  schema = {
+    username: Joi.string()
+      .required()
+      .label("Username"),
+    password: Joi.string()
+      .required()
+      .label("Password")
+  };
+
   handleSubmit = e => {
     e.preventDefault();
     const errors = this.validate();
-    console.log("errors", errors);
     this.setState({ errors: errors || {} });
     if (errors) return;
-    console.log("no errors so I can submit to the server or whatever");
+    //else call the server or whatever you want to do now there are no longer any errors.
   };
 
   handleChange = ({ currentTarget: input }) => {
@@ -27,21 +36,23 @@ class LoginForm extends Component {
     this.setState({ account, errors });
   };
   validateProperty = ({ name, value }) => {
-    if (name === "username") {
-      if (value.trim() === "") return "Username is Required.";
-    }
-    if (name === "password") {
-      if (value.trim() === "") return "Password is Required.";
-    }
+    const obj = { [name]: value };
+    const schema = { [name]: this.schema[name] };
+    const { error } = Joi.validate(obj, schema);
+    return error ? error.details[0].message : null;
   };
   validate = () => {
+    const result = Joi.validate(this.state.account, this.schema, {
+      abortEarly: false
+    });
+
+    //console.log(result);
+
+    if (!result.error) return null;
     const errors = {};
-    if (this.state.account.username.trim() === "")
-      errors.username = "Username is Required";
-    if (this.state.account.password.trim() === "")
-      errors.password = "Password is Required";
-    console.log("inside validate fgunction");
-    return Object.keys(errors).length === 0 ? null : errors;
+    for (let item of result.error.details) errors[item.path[0]] = item.message;
+
+    return errors;
   };
   render() {
     return (
@@ -64,7 +75,9 @@ class LoginForm extends Component {
             error={this.state.errors.password}
           />
 
-          <button className="btn btn-primary">Login</button>
+          <button disabled={this.validate()} className="btn btn-primary">
+            Login
+          </button>
         </form>
       </div>
     );

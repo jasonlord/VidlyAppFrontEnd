c52474aed715026c5745480cba8883601d47a0f7
Jason Lord <jasonjohnlord@gmail.com>
2020-02-26 14:07:26 -0500

    extracted the logic from loginForm into form which loginForm now extends

---
+++ src/components/form.jsx
@@ -0,0 +1,47 @@
+import React, { Component } from "react";
+import Joi from "joi-browser";
+
+class Form extends Component {
+  state = { data: {}, errors: {} };
+  validateProperty = ({ name, value }) => {
+    const obj = { [name]: value };
+    const schema = { [name]: this.schema[name] };
+    const { error } = Joi.validate(obj, schema);
+    return error ? error.details[0].message : null;
+  };
+  validate = () => {
+    const result = Joi.validate(this.state.data, this.schema, {
+      abortEarly: false
+    });
+
+    //console.log(result);
+
+    if (!result.error) return null;
+    const errors = {};
+    for (let item of result.error.details) errors[item.path[0]] = item.message;
+
+    return errors;
+  };
+  handleSubmit = e => {
+    e.preventDefault();
+    const errors = this.validate();
+    this.setState({ errors: errors || {} });
+    if (errors) return;
+
+    this.doSubmit();
+    //else call the server or whatever you want to do now there are no longer any errors.
+  };
+
+  handleChange = ({ currentTarget: input }) => {
+    const errors = { ...this.state.errors };
+    const errorMessage = this.validateProperty(input);
+    if (errorMessage) errors[input.name] = errorMessage;
+    else delete errors[input.name];
+
+    const data = { ...this.state.data };
+    data[input.name] = input.value;
+    this.setState({ data, errors });
+  };
+}
+
+export default Form;
--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,10 +1,11 @@
-import React, { Component } from "react";
-import Input from "./input";
+import React from "react";
 import Joi from "joi-browser";
+import Input from "./input";
+import Form from "./form";
 
-class LoginForm extends Component {
+class LoginForm extends Form {
   state = {
-    account: { username: "", password: "" },
+    data: { username: "", password: "" },
     errors: {}
   };
 
@@ -17,43 +18,10 @@ class LoginForm extends Component {
       .label("Password")
   };
 
-  handleSubmit = e => {
-    e.preventDefault();
-    const errors = this.validate();
-    this.setState({ errors: errors || {} });
-    if (errors) return;
-    //else call the server or whatever you want to do now there are no longer any errors.
+  doSubmit = () => {
+    console.log("no errors so now we can call the server or what have you..");
   };
 
-  handleChange = ({ currentTarget: input }) => {
-    const errors = { ...this.state.errors };
-    const errorMessage = this.validateProperty(input);
-    if (errorMessage) errors[input.name] = errorMessage;
-    else delete errors[input.name];
-
-    const account = { ...this.state.account };
-    account[input.name] = input.value;
-    this.setState({ account, errors });
-  };
-  validateProperty = ({ name, value }) => {
-    const obj = { [name]: value };
-    const schema = { [name]: this.schema[name] };
-    const { error } = Joi.validate(obj, schema);
-    return error ? error.details[0].message : null;
-  };
-  validate = () => {
-    const result = Joi.validate(this.state.account, this.schema, {
-      abortEarly: false
-    });
-
-    //console.log(result);
-
-    if (!result.error) return null;
-    const errors = {};
-    for (let item of result.error.details) errors[item.path[0]] = item.message;
-
-    return errors;
-  };
   render() {
     return (
       <div className="container containter-sm">
@@ -62,7 +30,7 @@ class LoginForm extends Component {
           <Input
             name="username"
             label="Username"
-            value={this.state.account.username}
+            value={this.state.data.username}
             onChange={this.handleChange}
             error={this.state.errors.username}
           />
@@ -70,7 +38,7 @@ class LoginForm extends Component {
           <Input
             name="password"
             lable="Password"
-            value={this.state.account.password}
+            value={this.state.data.password}
             onChange={this.handleChange}
             error={this.state.errors.password}
           />

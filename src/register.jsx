3f6fa68eb486b6dc6478d472502e4d31fcadc662
Jason Lord <jasonjohnlord@gmail.com>
2020-02-28 11:27:46 -0500

    added the input fields for the register page

--- src/register.jsx
+++ src/register.jsx
@@ -1,9 +1,46 @@
-import React, { Component } from "react";
+import React from "react";
+import Joi from "joi-browser";
 
-class Register extends Component {
-  state = {};
+import Form from "./components/form";
+
+class Register extends Form {
+  state = {
+    data: { username: "", password: "", name: "" },
+    errors: {}
+  };
+
+  schema = {
+    username: Joi.string()
+      .required()
+      .email({ minDomainAtoms: 2 })
+      .label("Username"),
+
+    password: Joi.string()
+      .required()
+      .min(5)
+      .max(20)
+      .label("Password"),
+    name: Joi.string()
+      .required()
+      .label("Name")
+  };
+
+  doSubmit = () => {
+    console.log("no errors so now we can call the server or what have you..");
+  };
+
   render() {
-    return <h1>Register</h1>;
+    return (
+      <div className="container containter-sm">
+        <h1>Register</h1>
+        <form onSubmit={this.handleSubmit}>
+          {this.renderInput("username", "Username")}
+          {this.renderInput("password", "Password", "password")}
+          {this.renderInput("name", "Name", "name")}
+          {this.renderButton("Register")}
+        </form>
+      </div>
+    );
   }
 }
 

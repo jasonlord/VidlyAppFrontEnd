5d8cf15d40bc00451c309ac63abd4cacce798d16
Jason Lord <jasonjohnlord@gmail.com>
2020-02-29 15:49:46 -0500

    got started on creating the movie form page, basic validation and temp select/dropdown box

--- src/components/movieForm.jsx
+++ src/components/movieForm.jsx
@@ -1,18 +1,63 @@
 import React from "react";
+import Joi from "joi-browser";
+import Input from "./input";
+import Form from "./form";
 
-const MovieForm = ({ match, history }) => {
-  return (
-    <div>
-      <h1>MovieForm {match.params.id}</h1>
-      <button
-        id="saveButtonMovieForm"
-        className="btn btn-primary"
-        onClick={() => history.push("/movies")}
-      >
-        Save
-      </button>
-    </div>
-  );
-};
+class MovieForm extends Form {
+  state = {
+    data: { username: "", password: "" },
+    errors: {}
+  };
+
+  schema = {
+    title: Joi.string()
+      .required()
+      .label("Title"),
+    numberInStock: Joi.string()
+      .required()
+      .label("Number In Stock")
+      .min(0)
+      .max(100),
+    rate: Joi.string()
+      .required()
+      .min(0)
+      .max(10)
+      .label("Rate")
+  };
+
+  doSubmit = () => {
+    console.log("no errors so now we can call the server or what have you..");
+  };
+
+  render() {
+    const { match, history } = this.props;
+
+    return (
+      <div className="container containter-sm">
+        <h1>Movie Id = {match.params.id}</h1>
+        <form onSubmit={this.handleSubmit}>
+          {this.renderInput("title", "Title")}
+          <div className="form-group">
+            <label htmlFor="genre">Genre</label>
+            <select id="genre" name="genre" class="custom-select">
+              <option value="rabbit">Rabbit</option>
+              <option value="duck">Duck</option>
+              <option value="fish">Fish</option>
+            </select>
+          </div>
+          {this.renderInput("numberInStock", "Number In Stock")}
+          {this.renderInput("rate", "Rate")}
+        </form>
+        <button
+          id="saveButtonMovieForm"
+          className="btn btn-primary"
+          onClick={() => history.push("/movies")}
+        >
+          Save
+        </button>
+      </div>
+    );
+  }
+}
 
 export default MovieForm;
